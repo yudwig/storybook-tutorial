2121ab83290de5b9aa7bb9b52ef37db2b9872e38
yudwig <yudwig.dev@gmail.com>
2021-02-28 13:21:31 +0900

    Add Proptypes to Task.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Task({task: {id, title, state}, onArchiveTask, onPinTask}) {
   return (
@@ -28,4 +29,12 @@ export default function Task({task: {id, title, state}, onArchiveTask, onPinTask
   );
 }
 
-
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
+};
